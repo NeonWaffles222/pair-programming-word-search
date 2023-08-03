b43340ca888edf2856803ecc4daffcbfc1d07363
Noah McMillan <noahm27@gmail.com>
2023-08-02 19:11:20 -0700

    Chacks if a word is in a crossword puzzle

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -24,15 +24,21 @@ describe("#wordSearch()", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['Y', 'F', 'C', 'F', 'H', 'U', 'A', 'L'],
       ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['W', 'H', 'C', 'S', 'L', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'L', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'O', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'HELLO');
 
     assert.isTrue(result);
+  });
+  it("should return false if the array is empty", function() {
+    const result = wordSearch([]
+      , '');
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+  if (!Array.isArray(letters)) {
+    return;
+  }
+
+  let arr = [];
+  for (let col = 0; col < letters.length; col++) {
+    let temp = [];
+    for (let row = 0; row < letters.length; row++) {
+      temp.push(letters[row][col]);
+    }
+    arr.push(temp);
+  }
+
+  let horizontal = hasWord(letters, word);
+  let vertical = hasWord(arr, word);
+
+  if (horizontal || vertical) {
+    return true;
+  }
+  return false;
+
+};
+
+const hasWord = (arr, word) => {
+  const join = arr.map(ls => ls.join(''));
+  for (let l of join) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
